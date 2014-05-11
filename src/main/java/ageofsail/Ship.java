2e632c5ba1a6bbd5fa0c2c28c1b034c3194853c6
penguinmenac3 <penguinmenac3@gmail.com>
2014-05-12 01:34:59 +0200

    Setup pom for java 1.8 and the project. added a dummy ship and test, no .empty required. ;)

---
+++ src/main/java/ageofsail/Ship.java
@@ -0,0 +1,8 @@
+package ageofsail;
+
+/**
+ * Created by PenguinMenace on 12.05.2014.
+ */
+public interface Ship {
+    // TODO
+}
---
+++ src/test/java/ShipTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by PenguinMenace on 12.05.2014.
+ */
+public class ShipTest {
+    // TODO
+}
